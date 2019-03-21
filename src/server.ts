629e4af237777bd1511e1f230a3636a59085fe27
JVI <you@example.com>
2019-03-21 22:27:36 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import {App} from './app';
+import {PostsController} from './controllers/posts/posts.controller';
+ 
+const app = new App(
+  [
+    new PostsController(),
+  ],
+  8080,
+);
+ 
+app.listen();
